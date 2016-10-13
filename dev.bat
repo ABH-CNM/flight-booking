19ac98cd417eb47bac3442c6baa2da208b25c3cb
chuchuz <kitechuz@gmail.com>
2016-10-14 01:31:09 +0700

    shortcut

---
+++ dev.bat
@@ -0,0 +1,12 @@
+echo off
+set arg=%1
+shift
+start cmd /k call "C:\Program Files\MongoDB\Server\3.2\bin\mongod" --dbpath "your-dbpath"
+
+if "%arg%" == "front" (
+    start cmd /k npm run watch-front
+    start cmd /k sass --watch --style expanded --cache-location ./tmp/.sass-cache public/scss/style.scss:public/css/style.css
+    npm server
+) else if "%arg%" == "back" (
+    npm run watch-back
+)
